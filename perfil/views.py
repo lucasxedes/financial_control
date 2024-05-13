860c473463079849e14160f2cfb1b040a941e93e
prxs <prxs@pop-os.localdomain>
2024-05-13 20:56:28 -0300

    feat: Implemeting gerenciar.html/views.gerenciar

--- perfil/views.py
+++ perfil/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
     if request.method == 'GET':
         return render(request, 'home.html')
+    
+def gerenciar(request):
+    if request.method == 'GET':
+        return render(request, 'gerenciar.html')
