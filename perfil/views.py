4c08e42f099e533d93b3443991fb10ad79eb9666
prxs <prxs@pop-os.localdomain>
2024-05-14 10:16:55 -0300

    feat: Implemeting logic cadastrar_banco

--- perfil/views.py
+++ perfil/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Conta
+from django.contrib import messages
+from django.contrib.messages import constants
+from django.http import HttpResponse
 
 def home(request):
     if request.method == 'GET':
@@ -7,3 +11,28 @@ def home(request):
 def gerenciar(request):
     if request.method == 'GET':
         return render(request, 'gerenciar.html')
+    
+def cadastrar_banco(request):
+    if request.method == 'POST':
+        apelido = request.POST.get('apelido')
+        banco = request.POST.get('banco')
+        tipo = request.POST.get('tipo')
+        valor = request.POST.get('valor')
+        icone = request.FILES.get('icone')
+        if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha o campo, apelido e valor')
+            return redirect('/perfil/gerenciar/')
+        try:
+            conta = Conta(
+                apelido=apelido,
+                banco=banco,
+                tipo=tipo,
+                valor=valor,
+                icone=icone
+            )
+            conta.save()
+            messages.add_message(request, constants.SUCCESS, 'Cadastrado com sucesso')
+            return redirect('/perfil/gerenciar/')
+        except Exception:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/perfil/gerenciar/')
